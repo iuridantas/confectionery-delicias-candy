1d04a7c1ccce88b9756e66198963750767aea939
iuridantas <uri.dantas@hotmail.com>
2023-10-04 11:26:22 -0300

    feat: start project

---
+++ src/styles/global.ts
@@ -0,0 +1,29 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+    font-family: 'Modern Antiqua', sans-serif;
+  }
+  
+  html, body, #root {
+    width: 100%;
+    height: 100%;
+  }
+  
+  body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #f2737a82;
+  color: #ffffff;
+}
+`;
+
+export const Content = styled.div`
+  align-items: center;
+  justify-content: center;
+`;
